24ea009446dab9b5dcf4e714110ab681d15572a1
Thiebaux <domino59242@gmail.com>
2026-07-29 14:43:53 +0200

    Renommage du site

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * site pour forcer les visiteurs à recharger les nouveaux fichiers.
  ******************************************************************************/
 
-const NOM_CACHE = "templeuve-interactive-v1";
+const NOM_CACHE = "Templeuve en Pévèle-v2";
 
 const FICHIERS_A_METTRE_EN_CACHE = [
     "./",
