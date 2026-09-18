a6fa4f354585a4a3c7da97210b70fe2f49190a66
Thiebaux <domino59242@gmail.com>
2026-09-18 13:51:09 +0200

    version 09

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * site pour forcer les visiteurs à recharger les nouveaux fichiers.
  ******************************************************************************/
 
-const NOM_CACHE = "templeuve-interactive-v08";
+const NOM_CACHE = "templeuve-interactive-v09";
 
 const FICHIERS_A_METTRE_EN_CACHE = [
     "./",
